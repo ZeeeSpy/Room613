678904a7ff204aae8671cd0c97fd37d86c187617
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-04-30 13:45:04 +0100

    Implement Save/Load

---
+++ Room613/Assets/MasterScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MasterScript: MonoBehaviour
+{
+	public static MasterScript instance;
+
+
+	private class SaveObject
+	{
+		public int test;
+	}
+
+	private void Start()
+	{
+		if (instance != this && instance != null) 
+		{
+			Destroy(gameObject);
+		} else
+		{
+			instance = this;
+			SaveUtil.Init();
+			DontDestroyOnLoad(this.gameObject);
+			//Only needs to be instantialized once
+		}
+		//Save after loading because this may be the player going back to the original scene
+		
+		//Load();
+		//Save();
+	}
+
+	public void Save()
+	{
+		//Collect Data
+		//Convert SaveObject to JSON
+		//Save JSON to text file
+
+
+		//Props in world
+		SaveObject SavedData = new SaveObject
+		{
+			test = 1,
+		};
+
+		string json = JsonUtility.ToJson(SavedData);
+		SaveUtil.Save(json);
+
+		//Save to text file for debugging
+		SaveUtil.TextSave(json);
+		Debug.Log("SAVED");
+	}
+
+	public void Load()
+	{
+		//Load Text File
+		//Convert from JSON to SaveObject
+		//From SaveObject Apply to world
+		string saveString = SaveUtil.Load();
+		if (saveString == null)
+		{
+			Debug.Log("NO SAVE DATA");
+		}
+		else
+		{
+			Debug.Log("SAVE DATA LOADED");
+		}
+
+		SaveObject SavedData = JsonUtility.FromJson<SaveObject>(saveString);
+
+
+		//Load things from SaveObject to world
+
+		//Current Order
+		//Load in props and put them in thier positions
+		//Load gold value
+		//Load last log in to see if theres a streak
+		//Load last known pet stats
+
+
+		//Load and instantiate props
+		int LoadedTest = SavedData.test;
+	}
+}
---
+++ Room613/Assets/SaveUtil.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using UnityEngine;
+
+public static class SaveUtil
+{
+	private static readonly string SAVE_FOLDER = Application.dataPath + "/Saves/";
+	private static readonly string PLAYER_SAVE_DATA = "PLAYER_SAVE_DATA";
+
+	public static void Init()
+	{
+		if (!Directory.Exists(SAVE_FOLDER)){
+			Directory.CreateDirectory(SAVE_FOLDER);
+		}
+	}
+
+	public static void TextSave(string SaveString)
+	{
+		File.WriteAllText(SAVE_FOLDER + "/save.txt", SaveString);
+	}
+
+	public static string TextLoad()
+	{
+		if (File.Exists(SAVE_FOLDER + "/save.txt"))
+		{
+			return File.ReadAllText(SAVE_FOLDER + "/save.txt");
+		} else
+		{
+			return null;
+		}
+	}
+
+	public static void Save(string SaveString)
+	{
+		PlayerPrefs.SetString(PLAYER_SAVE_DATA, SaveString);
+	}
+
+	public static string Load()
+	{
+		if (PlayerPrefs.GetString(PLAYER_SAVE_DATA,"null") != "null")
+		{
+			return PlayerPrefs.GetString(PLAYER_SAVE_DATA);
+		}
+		else
+		{
+			return null;
+		}
+	}
+}
